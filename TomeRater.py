c6f2c9c7ff99f304b2d6603bde954dc487464fe2
Thomas Grevedon <thomas.grevedon@gmail.com>
2018-06-07 21:23:38 +0200

    commit without change, just to synchronize git

--- TomeRater.py
+++ TomeRater.py
@@ -1,15 +1,20 @@
 class User(object):
     def __init__(self, name, email):
-        pass
+        self.name = name
+        self.email = email
+        self.books = {} #this will map a book object
+
 
     def get_email(self):
-        pass
+        return self.email
 
     def change_email(self, address):
-        pass
+        self.email = address
+        print("User's email has been updated")
 
     def __repr__(self):
-        pass
+        number_of_books_red = len(self.books)
+        return "User {user}, email {email}, books read: {books_red}".format(user = self.name, email = self.email, books_red = number_of_books_red)
 
     def __eq__(self, other_user):
         pass
