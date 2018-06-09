8f3c831f646929ae8d28df76791f2bcf1e766016
Thomas Grevedon <thomas.grevedon@gmail.com>
2018-06-09 13:23:37 +0200

    class Book has been defined and tested

--- TomeRater.py
+++ TomeRater.py
@@ -1,7 +1,7 @@
 class User(object):
     def __init__(self, name, email):
-        self.name = name
-        self.email = email
+        self.name = name #this will be a string
+        self.email = email #this will be a string
         self.books = {} #this will map a book object
 
 
@@ -17,10 +17,42 @@ class User(object):
         return "User {user}, email {email}, books read: {books_red}".format(user = self.name, email = self.email, books_red = number_of_books_red)
 
     def __eq__(self, other_user):
-        return other_user.name == self.name
-        return other_user.email == self.email
+        if other_user.name == self.name:
+            return other_user.email == self.email
 
+class Book(object):
+    def __init__(self, title, isbn):
+        self.title = title #this will be a string
+        self.isbn = isbn #this will be a number
+        self.rating = []
 
-user1 = User("Thomas", "thomas.grevedon@gmail.com")
-user2 = User("Thoma", "thomas.grevedon@gmail.com")
-print(user1 == user2)
+    def get_title(self):
+        return self.title
+
+    def get_isbn(self):
+        return self.isbn
+
+    def set_isbn(self, new_isbn):
+        self.isbn = new_isbn
+        print("the book's ISBN has been updated")
+
+    def add_rating(self, rating):
+        if rating >= 0 and rating <= 4:
+            self.rating.append(rating)
+        else:
+            print("invalid Rating")
+
+    def __eq__(self, other_book):
+        if other_book.title == self.title:
+            return other_book.isbn == self.isbn
+
+book1 = Book("aladin", 1234568)
+book2 = Book("aladin", 1234567)
+print(book1.title)
+print(book1.isbn)
+print(book1 == book2)
+book1.set_isbn(1234567)
+print(book1 == book2)
+book1.add_rating(5)
+book1.add_rating(3)
+print(book1.rating)
